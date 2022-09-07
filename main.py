d8caf5f1955b388f0bf8995904c163135f52dd95
Daniel-Aguila <danielbaguila@gmail.com>
2022-09-07 13:25:51 -0500

    dropped location feature, replaced na with mode word, started tokenization

---
+++ main.py
@@ -0,0 +1,40 @@
+from statistics import mode
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
+
+#get the data
+print(train_data.shape)
+print(train_data.head())
+
+#how much is missing
+
+def missingData():
+    missing_test_data = train_data.isnull().sum()
+    print(missing_test_data)
+
+#given that a lot of values in the location column are not real places or are either to general like "Milky Way". Plus
+#adding on to the fact that anyone at any location can tweet of a disaster from another location in the world, and that a disaster
+#can happen anywhere on earth gives to my opinion that the column should be drop
+
+train_data = train_data.drop(columns=['location'])
+test_data = test_data.drop(columns=['location'])
+
+print("keyword mode value:", str(train_data['keyword'].mode()))
+train_data_keyword_mode = train_data['keyword'].mode()
+test_data_keyword_mode = test_data['keyword'].mode()
+#we fill the na values on keyword with the mode
+train_data['keyword'] = train_data['keyword'].fillna(str(train_data_keyword_mode))
+test_data['keyword'] = test_data['keyword'].fillna(str(test_data_keyword_mode))
+
+train_features = train_data.copy()
+train_labels = train_features.pop('target')
+
+tokenizer = Tokenizer(num_words = 20, oov_token = "<OOV>")
+tokenizer.fit_on_texts(train_features['text'])
+word_index = tokenizer.word_index
+print(word_index)
+
